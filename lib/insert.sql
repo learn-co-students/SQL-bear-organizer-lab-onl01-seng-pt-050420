052e6cc0756eb67f842c85f0ac57f4b5f6dd7f07
cedric-joaquin <joaquin.cedric@gmail.com>
2020-06-29 22:55:42 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color) VALUES ("Mr. Chocolate", 98, "Male", "Brown"); 
+INSERT INTO bears (name, age, gender, color) VALUES ("Atlas", 133, "Male", "Brown"); 
+INSERT INTO bears (name, age, gender, color) VALUES ("Supreme", 5, "Male", "Red"); 
+INSERT INTO bears (name, age, gender, color) VALUES ("Melissa", 25, "Female", "White") ;
+INSERT INTO bears (name, age, gender, color) VALUES ("Teddy", 49, "Female", "Black"); 
+INSERT INTO bears (name, age, gender, color) VALUES ("Bossman", 1, "Female", "Brown"); 
+INSERT INTO bears (name, age, gender, color) VALUES ("Rudy", 12, "Male", "White"); 
+INSERT INTO bears (name, age, gender, color) VALUES (NULL, 13, "Male", "Brown"); 
